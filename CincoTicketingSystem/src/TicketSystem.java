fe08facbc01bfaea2c35fd55732c827f0b3fb52a
Lewis Martin <s3775633@student.rmit.edu.au>
2021-04-07 22:15:18 +1000

    Added code comments

--- CincoTicketingSystem/src/TicketSystem.java
+++ CincoTicketingSystem/src/TicketSystem.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 
+// Class used to represent the entire system
 public class TicketSystem {
+	//Arrays of staff and technicians
 	private ArrayList<Technician> technicians;
 	private ArrayList<Staff> staff;
 	
+	//Ticket system constructor
 	public TicketSystem()
 	{
 		technicians = new ArrayList<Technician>();
 		staff = new ArrayList<Staff>();
 	}
 	
+	// Getters
 	public ArrayList<Technician> getTechnicians()
 	{
 		return technicians;
@@ -20,6 +24,7 @@ public class TicketSystem {
 		return staff;
 	}
 	
+	// Method to add a technician to the technicians array
 	public void addTechnician(Technician technician)
 	{
 		technicians.add(technician);
